a84865534ec565da91632b455b51610c10b1d51a
Rik Baehnemann <brik@student.ethz.ch>
2017-04-20 15:35:06 +0200

    More constructors.

--- mav_trajectory_generation_ros/include/mav_trajectory_generation_ros/feasibility_base.h
+++ mav_trajectory_generation_ros/include/mav_trajectory_generation_ros/feasibility_base.h
@@ -21,10 +21,10 @@
 #ifndef MAV_TRAJECTORY_GENERATION_FEASIBILITY_BASE_H_
 #define MAV_TRAJECTORY_GENERATION_FEASIBILITY_BASE_H_
 
-#include <Eigen/Core>
-#include <Eigen/StdVector>
 #include <glog/logging.h>
 #include <ros/ros.h>
+#include <Eigen/Core>
+#include <Eigen/StdVector>
 
 namespace mav_trajectory_generation {
 const double kGravity = 9.81;  // [m/s/s]
@@ -33,12 +33,19 @@ const double kGravity = 9.81;  // [m/s/s]
 struct InputConstraints {
   // Reasonable default constraints.
   InputConstraints();
+  InputConstraints(double f_min, double f_max, double v_max,
+                   double omega_xy_max, double omega_z_max,
+                   double omega_dot_z_max);
+
+  // TODO(rikba): Make class and use setter and getter to check user input,
+  // e.g., 0 < f_min < f_max?
+
+  // Minimum input acceleration (mass normalized thrust) [m/s/s].
+  double f_min;
+  // Maximum input acceleration (mass normalized thrust) [m/s/s].
+  double f_max;
   // Maximum velocity [m/s].
   double v_max;
-  // Minimum input acceleration (mass free) [m/s/s].
-  double f_min;
-  // Maximum input acceleration (mass free) [m/s/s].
-  double f_max;
   // Maximum roll/pitch input rate [rad/s].
   double omega_xy_max;
   // Maximum yaw rate [rad/s].
@@ -54,7 +61,8 @@ class HalfPlane {
   typedef std::vector<HalfPlane, Eigen::aligned_allocator<HalfPlane>> Vector;
   HalfPlane(const Eigen::Vector3d& point, const Eigen::Vector3d& normal);
   // Define the half plane from 3 counter-clockwise points (seen from above).
-  HalfPlane(const Eigen::Vector3d& a, const Eigen::Vector3d& b, const Eigen::Vector3d& c);
+  HalfPlane(const Eigen::Vector3d& a, const Eigen::Vector3d& b,
+            const Eigen::Vector3d& c);
 
  private:
   Eigen::Vector3d point_;
@@ -65,6 +73,10 @@ class HalfPlane {
 // feasibility checks.
 class FeasibilityBase {
  public:
+  // Default input constraints, no half plane constraints.
+  FeasibilityBase();
+  // User input constraints, no half plane constraints.
+  FeasibilityBase(const InputConstraints& input_constraints);
   // Checks a segment for input feasibility.
   inline virtual bool checkInputFeasibility() {
     ROS_ERROR_STREAM("Input feasibility check not implemented.");
@@ -73,7 +85,7 @@ class FeasibilityBase {
 
   // Checks if a segment stays within a set of half planes.
   inline virtual bool checkHalfPlaneFeasibility() {
-    ROS_ERROR_STREAM("Bounding box feasibility check not implemented.");
+    ROS_ERROR_STREAM("Half plane feasibility check not implemented.");
     return false;
   }
 
--- mav_trajectory_generation_ros/src/feasibility_base.cpp
+++ mav_trajectory_generation_ros/src/feasibility_base.cpp
@@ -25,24 +25,47 @@
 namespace mav_trajectory_generation {
 
 InputConstraints::InputConstraints()
-    : v_max(4.0),
-      f_min(0.5 * kGravity),
+    : f_min(0.5 * kGravity),
       f_max(1.5 * kGravity),
+      v_max(4.0),
       omega_xy_max(M_PI),
       omega_z_max(M_PI / 2.0),
-      omega_dot_z_max(2.0 * M_PI){};
+      omega_dot_z_max(2.0 * M_PI) {}
+
+InputConstraints::InputConstraints(double f_min, double f_max, double v_max,
+                                   double omega_xy_max, double omega_z_max,
+                                   double omega_dot_z_max)
+    : f_min(f_min),
+      f_max(f_max),
+      v_max(v_max),
+      omega_xy_max(omega_xy_max),
+      omega_z_max(omega_z_max),
+      omega_dot_z_max(omega_dot_z_max) {
+  // Some sanity checks.
+  CHECK_GT(f_min, 0.0);
+  CHECK_GT(f_max, 0.0);
+  CHECK_GT(f_max, f_min);
+
+  CHECK_GT(v_max, 0.0);
+  CHECK_GT(omega_xy_max, 0.0);
+  CHECK_GT(omega_z_max, 0.0);
+  CHECK_GT(omega_dot_z_max, 0.0);
+}
 
 HalfPlane::HalfPlane(const Eigen::Vector3d& point,
                      const Eigen::Vector3d& normal)
-    : point_(point), normal_(normal){
-      CHECK_GT(normal.norm(), 0.0) << "Invalid normal.";
-      normal_.normalize();
-    };
+    : point_(point), normal_(normal) {
+  CHECK_GT(normal.norm(), 0.0) << "Invalid normal.";
+  normal_.normalize();
+}
 
 HalfPlane::HalfPlane(const Eigen::Vector3d& a, const Eigen::Vector3d& b,
                      const Eigen::Vector3d& c) {
   point_ = a;
   normal_ = (b - a).cross(c - a).normalized();
 }
+
+FeasibilityBase::FeasibilityBase(const InputConstraints& input_constraints)
+    : input_constraints_(input_constraints) {}
 
 }  // namespace mav_trajectory_generation
