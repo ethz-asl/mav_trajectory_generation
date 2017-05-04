f7e701e77dfa12fc86bd777cea9690365ce8d333
Rik Baehnemann <brik@student.ethz.ch>
2017-05-04 10:45:56 +0200

    Format.

--- mav_visualization/include/mav_visualization/helpers.h
+++ mav_visualization/include/mav_visualization/helpers.h
@@ -21,34 +21,34 @@
 #include <Eigen/Eigenvalues>
 
 #include <eigen_conversions/eigen_msg.h>
-#include <visualization_msgs/MarkerArray.h>
 #include <std_msgs/ColorRGBA.h>
+#include <visualization_msgs/MarkerArray.h>
 
 namespace mav_visualization {
 
 class Color : public std_msgs::ColorRGBA {
-public:
-Color() : std_msgs::ColorRGBA() {}
-Color(double red, double green, double blue) : Color(red, green, blue, 1.0) {}
-Color(double red, double green, double blue, double alpha) : Color() {
-r = red;
-g = green;
-b = blue;
-a = alpha;
-}
+ public:
+  Color() : std_msgs::ColorRGBA() {}
+  Color(double red, double green, double blue) : Color(red, green, blue, 1.0) {}
+  Color(double red, double green, double blue, double alpha) : Color() {
+    r = red;
+    g = green;
+    b = blue;
+    a = alpha;
+  }
 
-static const Color White() { return Color(1.0, 1.0, 1.0); }
-static const Color Black() { return Color(0.0, 0.0, 0.0); }
-static const Color Gray() { return Color(0.5, 0.5, 0.5); }
-static const Color Red() { return Color(1.0, 0.0, 0.0); }
-static const Color Green() { return Color(0.0, 1.0, 0.0); }
-static const Color Blue() { return Color(0.0, 0.0, 1.0); }
-static const Color Yellow() { return Color(1.0, 1.0, 0.0); }
-static const Color Orange() { return Color(1.0, 0.5, 0.0); }
-static const Color Purple() { return Color(0.5, 0.0, 1.0); }
-static const Color Chartreuse() { return Color(0.5, 1.0, 0.0); }
-static const Color Teal() { return Color(0.0, 1.0, 1.0); }
-static const Color Pink() { return Color(1.0, 0.0, 0.5); }
+  static const Color White() { return Color(1.0, 1.0, 1.0); }
+  static const Color Black() { return Color(0.0, 0.0, 0.0); }
+  static const Color Gray() { return Color(0.5, 0.5, 0.5); }
+  static const Color Red() { return Color(1.0, 0.0, 0.0); }
+  static const Color Green() { return Color(0.0, 1.0, 0.0); }
+  static const Color Blue() { return Color(0.0, 0.0, 1.0); }
+  static const Color Yellow() { return Color(1.0, 1.0, 0.0); }
+  static const Color Orange() { return Color(1.0, 0.5, 0.0); }
+  static const Color Purple() { return Color(0.5, 0.0, 1.0); }
+  static const Color Chartreuse() { return Color(0.5, 1.0, 0.0); }
+  static const Color Teal() { return Color(0.0, 1.0, 1.0); }
+  static const Color Pink() { return Color(1.0, 0.0, 0.5); }
 };
 
 /// helper function to create a geometry_msgs::Point
