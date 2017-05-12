934705b600079586816eb43c9f0de4a6c94fbce3
Rik Baehnemann <brik@student.ethz.ch>
2017-05-12 12:50:05 +0200

    Autoformat.

--- mav_trajectory_generation/src/vertex.cpp
+++ mav_trajectory_generation/src/vertex.cpp
@@ -169,9 +169,9 @@ std::vector<double> estimateSegmentTimes(const Vertex::Vector& vertices,
     vertices[i].getConstraint(derivative_order::POSITION, &start);
     vertices[i + 1].getConstraint(derivative_order::POSITION, &end);
     double distance = (end - start).norm();
-    double t = distance / v_max * 2 * (1.0 +
-                                       magic_fabian_constant * v_max / a_max *
-                                           exp(-distance / v_max * 2));
+    double t = distance / v_max * 2 *
+               (1.0 + magic_fabian_constant * v_max / a_max *
+                          exp(-distance / v_max * 2));
     segment_times.push_back(t);
   }
   return segment_times;
