cca533acbea2b67030fcadf892193a89d5ef6513
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2018-11-20 16:21:28 +0100

    added docs page

--- mav_trajectory_generation/include/mav_trajectory_generation/segment.h
+++ mav_trajectory_generation/include/mav_trajectory_generation/segment.h
@@ -9,7 +9,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * http:///www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -36,10 +36,10 @@ namespace mav_trajectory_generation {
 constexpr double kNumNSecPerSec = 1.0e9;
 constexpr double kNumSecPerNsec = 1.0e-9;
 
-// Class holding the properties of parametric segment of a path.
-// Time of the segment and one polynomial for each dimension.
-//    X------------X---------------X
-//  vertex             segment
+/// Class holding the properties of parametric segment of a path.
+/// Time of the segment and one polynomial for each dimension.
+///    X------------X---------------X
+///  vertex             segment
 class Segment {
  public:
   typedef std::vector<Segment> Vector;
@@ -71,47 +71,45 @@ class Segment {
   Eigen::VectorXd evaluate(
       double t, int derivative_order = derivative_order::POSITION) const;
 
-  // Computes the candidates for the minimum and maximum magnitude of a single
-  // segment in the specified derivative. In the 1D case, it simply returns the
-  // roots of the derivative of the segment-polynomial. For higher dimensions,
-  // e.g. 3D, we need to find the extrema of \sqrt{x(t)^2 + y(t)^2 + z(t)^2}
-  // where x, y, z are polynomials describing the position or the derivative,
-  // specified by Derivative. Taking the derivative yields
-  // 2 x \dot{x} + 2 y \dot{y} + 2 z \dot{z}, which needs to be zero at the
-  // extrema. The multiplication of two polynomials is a convolution of their
-  // coefficients. Re-ordering by their powers and addition yields a polynomial,
-  // for which we can compute the roots.
-  // Input: derivative = Derivative of position, in which to find the maxima.
-  // Input: t_start = Only maxima >= t_start are returned. Usually set to 0.
-  // Input: t_stop = Only maxima <= t_stop are returned. Usually set to segment
-  // time.
-  // Input: dimensions = Vector containing the dimensions that are evaluated.
-  // Usually [0, 1, 2] for position, [3] for yaw.
-  // Output: candidates = Vector containing the candidate extrema times.
-  // Returns whether the computation succeeded -- false means no candidates
-  // were found by Jenkins-Traub.
+  /// Computes the candidates for the minimum and maximum magnitude of a single
+  /// segment in the specified derivative. In the 1D case, it simply returns the
+  /// roots of the derivative of the segment-polynomial. For higher dimensions,
+  /// e.g. 3D, we need to find the extrema of \sqrt{x(t)^2 + y(t)^2 + z(t)^2}
+  /// where x, y, z are polynomials describing the position or the derivative,
+  /// specified by Derivative. Taking the derivative yields
+  /// 2 x \dot{x} + 2 y \dot{y} + 2 z \dot{z}, which needs to be zero at the
+  /// extrema. The multiplication of two polynomials is a convolution of their
+  /// coefficients. Re-ordering by their powers and addition yields a
+  /// polynomial, for which we can compute the roots. Input: derivative =
+  /// Derivative of position, in which to find the maxima. Input: t_start = Only
+  /// maxima >= t_start are returned. Usually set to 0. Input: t_stop = Only
+  /// maxima <= t_stop are returned. Usually set to segment time. Input:
+  /// dimensions = Vector containing the dimensions that are evaluated. Usually
+  /// [0, 1, 2] for position, [3] for yaw. Output: candidates = Vector
+  /// containing the candidate extrema times. Returns whether the computation
+  /// succeeded -- false means no candidates were found by Jenkins-Traub.
   bool computeMinMaxMagnitudeCandidateTimes(
       int derivative, double t_start, double t_end,
       const std::vector<int>& dimensions,
       std::vector<double>* candidate_times) const;
 
-  // Convenience function. Additionally evaluates the candidate times.
+  /// Convenience function. Additionally evaluates the candidate times.
   bool computeMinMaxMagnitudeCandidates(
       int derivative, double t_start, double t_end,
       const std::vector<int>& dimensions,
       std::vector<Extremum>* candidates) const;
 
-  // Convenience function. Evaluates the magnitudes between t_start and t_end
-  // for a set of candidates for given dimensions.
+  /// Convenience function. Evaluates the magnitudes between t_start and t_end
+  /// for a set of candidates for given dimensions.
   bool selectMinMaxMagnitudeFromCandidates(
       int derivative, double t_start, double t_end,
       const std::vector<int>& dimensions,
       const std::vector<Extremum>& candidates, Extremum* minimum,
       Extremum* maximum) const;
 
-  // Split a segment to get a segment with the specified dimension.
+  /// Split a segment to get a segment with the specified dimension.
   bool getSegmentWithSingleDimension(int dimension, Segment* new_segment) const;
-  // Compose this segment and another segment to a new segment.
+  /// Compose this segment and another segment to a new segment.
   bool getSegmentWithAppendedDimension(const Segment& segment_to_append,
                                        Segment* new_segment) const;
 
@@ -120,13 +118,13 @@ class Segment {
   double time_;
 
  private:
-  int N_;  // Number of coefficients.
-  int D_;  // Number of dimensions.
+  int N_;  ///< Number of coefficients.
+  int D_;  ///< Number of dimensions.
 };
 
-// Prints the properties of the segment.
-// Polynomial coefficients are printed with increasing powers,
-// i.e. c_0 + c_1*t ... c_{N-1} * t^{N-1}
+/// Prints the properties of the segment.
+/// Polynomial coefficients are printed with increasing powers,
+/// i.e. c_0 + c_1*t ... c_{N-1} * t^{N-1}
 void printSegment(std::ostream& stream, const Segment& s, int derivative);
 
 std::ostream& operator<<(std::ostream& stream, const Segment& s);
